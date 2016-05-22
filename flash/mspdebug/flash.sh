331eefc0e3cbb55cf00c5f98913b82abfd19ce11
kyleparrott <kyle.parrott@gmail.com>
2016-05-22 11:51:10 -0500

    allow firmware update during msp flashing

--- flash/mspdebug/flash.sh
+++ flash/mspdebug/flash.sh
@@ -4,5 +4,5 @@ mspdebug=$1
 cmd=$2
 exe=$3
 
-echo $mspdebug tilib \"$cmd\" $exe
-$mspdebug tilib "$cmd $exe" 
+echo $mspdebug tilib \"$cmd\" $exe --allow-fw-update
+$mspdebug tilib "$cmd $exe" --allow-fw-update
