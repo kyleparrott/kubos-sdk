9689da9d548233c7740e9033ebac919c65641008
kyleparrott <kyle.parrott@gmail.com>
2016-06-29 10:33:41 -0500

    change dfu-util flash script to only flash the .bin instead of elf project binary

--- flash/dfu_util/flash.sh
+++ flash/dfu_util/flash.sh
@@ -2,6 +2,6 @@
 dfu_util=$1
 exe=$2
 
-echo $dfu_util --alt 0 -D $exe -i 0 -s 0x08000000
-$dfu_util --alt 0 -D $exe -i 0 -s 0x08000000
+echo $dfu_util --alt 0 -D $exe.bin -i 0 -s 0x08000000
+$dfu_util --alt 0 -D $exe.bin -i 0 -s 0x08000000
 
