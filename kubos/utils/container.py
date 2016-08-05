9d3438ac324fb95710362400622548c718be49ab
Ryan Plauche <ryan@kubos.co>
2016-08-05 13:57:33 -0500

    v0.0.5

--- kubos/utils/container.py
+++ kubos/utils/container.py
@@ -25,7 +25,7 @@ from docker import Client
 from project import get_local_link_file
 
 container_repo = 'kubostech/kubos-sdk'
-container_tag = '0.0.4'
+container_tag = '0.0.5'
 
 def get_cli():
     if sys.platform.startswith('linux'):
