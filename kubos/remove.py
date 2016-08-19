d110ad15a72d5af3a63aa79abb3d0542dc3390c8
kyleparrott <kyle@kubos.co>
2016-08-19 10:12:59 -0500

    unlink targets and modules with

--- kubos/remove.py
+++ kubos/remove.py
@@ -20,28 +20,36 @@ import sys
 
 from options import parser
 from utils import container
-from utils.project import get_local_link_file
+from utils.project import get_local_link_file, module_key, target_key
 
 def addOptions(parser):
     parser.add_argument('module_name', nargs=1, help='remove a previously linked module from the local build')
 
 
 def execCommand(args, following_args):
-    module_name = args.module_name[0]
+    module_or_target = args.module_name[0]
     local_link_file = get_local_link_file()
     if os.path.isfile(local_link_file):
         with open(local_link_file, 'r') as link_file:
             link_data = json.load(link_file)
-        if module_name in link_data:
-            link_data.pop(module_name)
-            with open(local_link_file, 'w') as link_file:
-                links = json.dumps(link_data)
-                link_file.write(links)
-                print 'Successfully unlinked %s' % module_name
+            module_data = link_data[module_key]
+            target_data = link_data[target_key]
+        if module_or_target in module_data:
+            remove_link(module_or_target, module_key, link_data)
+            print "Successfully removed linked module %s" % module_or_target
+        elif module_or_target in target_data:
+            remove_link(module_or_target, target_key, link_data)
+            print "Successfully removed linked target %s" % module_or_target
         else:
-            print >>sys.stderr, 'Module %s is not currently linked' % module_name
+            print >>sys.stderr, 'Module or target %s is not currently linked' % module_or_target
             sys.exit(1)
     else:
-        print >>sys.stderr, 'No modules are currently linked. Nothing to unlink.'
+        print >>sys.stderr, 'No modules or targets are currently linked. Nothing to unlink.'
         sys.exit(1)
+
+def remove_link(module_or_target, json_key, json_data):
+    json_data[json_key].pop(module_or_target)
+    local_link_file = get_local_link_file()
+    with open(local_link_file, 'w') as link_file:
+        link_file.write(json.dumps(json_data))
 
