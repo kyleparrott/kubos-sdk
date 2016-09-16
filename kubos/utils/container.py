bc019263eaeac74baab58b9817690c5d7c27483f
kyleparrott <kyle@kubos.co>
2016-09-16 09:55:34 -0500

    Remove fix_permissions call, using the new base sdk container resolves the need for it. Move mounting directories into the container creation to allow compatability with docker-py > 1.9.0

--- kubos/utils/container.py
+++ kubos/utils/container.py
@@ -53,9 +53,11 @@ def pass_through(*args):
     arg_list = list(args)
     arg_list.insert(0, python)
     arg_list.insert(1, sdk_script)
-    run_container(arg_list)
-    if sys.platform.startswith('linux'):
-        fix_permissions()
+    try:
+        run_container(arg_list)
+    except docker.errors.NotFound:
+        print "The correct container was not found"
+        print "Please run `kubos update` and try again"
 
 
 def fix_permissions():
@@ -72,8 +74,16 @@ def fix_permissions():
 def run_container(arg_list):
     cwd = os.getcwd()
     cli = get_cli()
-    image_name = "%s:%s" % (container_repo, container_tag)
-    container_data = cli.create_container(image=image_name, command=arg_list, working_dir=cwd, tty=True)
+
+    image_name = "%s:%s" % (container_repo, container_tag())
+    host_config = cli.create_host_config(binds=mount_volumes())
+    uid_var = get_uid()
+    container_data = cli.create_container(image=image_name,
+                                          command=arg_list,
+                                          environment=get_uid(),
+                                          host_config=host_config,
+                                          working_dir=cwd,
+                                          tty=True)
     container_id = container_data['Id'].encode('utf8')
     if container_data['Warnings']:
         print "Warnings: ", container_data['Warnings']
@@ -87,6 +97,14 @@ def run_container(arg_list):
     cli.stop(container_id)
     cli.remove_container(container_id)
     status_spinner.stop_spinner(spinner)
+
+
+def get_uid():
+    '''This returns an array with the current user id in a single element string array
+    This is used in the container so that the user in the container has the same uid
+    as the user on the host to avoid permission errors after container commands run'''
+    uid = os.getuid()
+    return ['LOCAL_USER_ID=%s' % uid]
 
 def json_events(iter):
     # docker-py inconsistently streams json data through the iterator interface,
